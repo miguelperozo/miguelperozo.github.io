ab62ef05a0d0f71973a84e98de1e92c37b8f2150
miguelperozo <perozotorrents@gmail.com>
2025-04-27 22:05:35 +0200

    Update Text

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -41,7 +41,7 @@ export type SiteConfig = {
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
     title: 'Miguel Perozo',
-    subtitle: 'Welcome to my personal website! ',
+    subtitle: 'Discover My Journey in the World of Technology and Design ',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/post-3.jpg',
@@ -98,7 +98,7 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
+        title: 'Hi There & Welcome to My Digital Space!',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/post-3.jpg',
